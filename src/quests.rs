8b9ed128632c503099c1dacfaef468384202b893
Marshall Bowers <elliott.codes@gmail.com>
2020-05-14 22:53:22 -0400

    Use strings for quest IDs

--- src/quests.rs
+++ src/quests.rs
@@ -13,7 +13,7 @@ lazy_static! {
 }
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
-pub struct QuestId(i32);
+pub struct QuestId(&'static str);
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
 pub enum QuestKind {
@@ -92,323 +92,321 @@ pub enum QuestRequirement {
     QuestPoints(u32),
 }
 
-pub const COOKS_ASSISTANT: QuestId = QuestId(1);
+pub const COOKS_ASSISTANT: QuestId = QuestId("cooks_assistant");
 
-pub const DEMON_SLAYER: QuestId = QuestId(2);
+pub const DEMON_SLAYER: QuestId = QuestId("demon_slayer");
 
-pub const THE_RESTLESS_GHOST: QuestId = QuestId(3);
+pub const THE_RESTLESS_GHOST: QuestId = QuestId("the_restless_ghost");
 
-pub const ROMEO_AND_JULIET: QuestId = QuestId(4);
+pub const ROMEO_AND_JULIET: QuestId = QuestId("romeo_and_juliet");
 
-pub const SHEEP_SHEARER: QuestId = QuestId(5);
+pub const SHEEP_SHEARER: QuestId = QuestId("sheep_shearer");
 
-pub const SHIELD_OF_ARRAV: QuestId = QuestId(6);
+pub const SHIELD_OF_ARRAV: QuestId = QuestId("shield_of_arrav");
 
-pub const ERNEST_THE_CHICKEN: QuestId = QuestId(7);
+pub const ERNEST_THE_CHICKEN: QuestId = QuestId("ernest_the_chicken");
 
-pub const VAMPIRE_SLAYER: QuestId = QuestId(8);
+pub const VAMPIRE_SLAYER: QuestId = QuestId("vampire_slayer");
 
-pub const IMP_CATCHER: QuestId = QuestId(9);
+pub const IMP_CATCHER: QuestId = QuestId("imp_catcher");
 
-pub const PRINCE_ALI_RESCUE: QuestId = QuestId(10);
+pub const PRINCE_ALI_RESCUE: QuestId = QuestId("prince_ali_rescue");
 
-pub const DORICS_QUEST: QuestId = QuestId(11);
+pub const DORICS_QUEST: QuestId = QuestId("dorics_quest");
 
-pub const BLACK_KNIGHTS_FORTRESS: QuestId = QuestId(12);
+pub const BLACK_KNIGHTS_FORTRESS: QuestId = QuestId("black_knights_fortress");
 
-pub const WITCHS_POTION: QuestId = QuestId(13);
+pub const WITCHS_POTION: QuestId = QuestId("witchs_potion");
 
-pub const THE_KNIGHTS_SWORD: QuestId = QuestId(14);
+pub const THE_KNIGHTS_SWORD: QuestId = QuestId("the_knights_sword");
 
-pub const GOBLIN_DIPLOMACY: QuestId = QuestId(15);
+pub const GOBLIN_DIPLOMACY: QuestId = QuestId("goblin_diplomacy");
 
-pub const PIRATES_TREASURE: QuestId = QuestId(16);
+pub const PIRATES_TREASURE: QuestId = QuestId("pirates_treasure");
 
-pub const DRAGON_SLAYER: QuestId = QuestId(17);
+pub const DRAGON_SLAYER: QuestId = QuestId("dragon_slayer");
 
-pub const DRUIDIC_RITUAL: QuestId = QuestId(18);
+pub const DRUIDIC_RITUAL: QuestId = QuestId("druidic_ritual");
 
-pub const LOST_CITY: QuestId = QuestId(19);
+pub const LOST_CITY: QuestId = QuestId("lost_city");
 
-pub const WITCHS_HOUSE: QuestId = QuestId(20);
+pub const WITCHS_HOUSE: QuestId = QuestId("witchs_house");
 
-pub const MERLINS_CRYSTAL: QuestId = QuestId(21);
+pub const MERLINS_CRYSTAL: QuestId = QuestId("merlins_crystal");
 
-pub const HEROES_QUEST: QuestId = QuestId(22);
+pub const HEROES_QUEST: QuestId = QuestId("heroes_quest");
 
-pub const SCORPION_CATCHER: QuestId = QuestId(23);
+pub const SCORPION_CATCHER: QuestId = QuestId("scorpion_catcher");
 
-pub const FAMILY_CREST: QuestId = QuestId(24);
+pub const FAMILY_CREST: QuestId = QuestId("family_crest");
 
-pub const TRIBAL_TOTEM: QuestId = QuestId(25);
+pub const TRIBAL_TOTEM: QuestId = QuestId("tribal_totem");
 
-pub const FISHING_CONTEST: QuestId = QuestId(26);
+pub const FISHING_CONTEST: QuestId = QuestId("fishing_contest");
 
-pub const MONKS_FRIEND: QuestId = QuestId(27);
+pub const MONKS_FRIEND: QuestId = QuestId("monks_friend");
 
-pub const TEMPLE_OF_IKOV: QuestId = QuestId(28);
+pub const TEMPLE_OF_IKOV: QuestId = QuestId("temple_of_ikov");
 
-pub const CLOCK_TOWER: QuestId = QuestId(29);
+pub const CLOCK_TOWER: QuestId = QuestId("clock_tower");
 
-pub const HOLY_GRAIL: QuestId = QuestId(30);
+pub const HOLY_GRAIL: QuestId = QuestId("holy_grail");
 
-pub const TREE_GNOME_VILLAGE: QuestId = QuestId(31);
+pub const TREE_GNOME_VILLAGE: QuestId = QuestId("tree_gnome_village");
 
-pub const FIGHT_ARENA: QuestId = QuestId(32);
+pub const FIGHT_ARENA: QuestId = QuestId("fight_arena");
 
-pub const HAZEEL_CULT: QuestId = QuestId(33);
+pub const HAZEEL_CULT: QuestId = QuestId("hazeel_cult");
 
-pub const SHEEP_HERDER: QuestId = QuestId(34);
+pub const SHEEP_HERDER: QuestId = QuestId("sheep_herder");
 
-pub const PLAGUE_CITY: QuestId = QuestId(35);
+pub const PLAGUE_CITY: QuestId = QuestId("plague_city");
 
-pub const SEA_SLUG: QuestId = QuestId(36);
+pub const SEA_SLUG: QuestId = QuestId("sea_slug");
 
-pub const WATERFALL_QUEST: QuestId = QuestId(37);
+pub const WATERFALL_QUEST: QuestId = QuestId("waterfall_quest");
 
-pub const BIOHAZARD: QuestId = QuestId(38);
+pub const BIOHAZARD: QuestId = QuestId("biohazard");
 
-pub const JUNGLE_POTION: QuestId = QuestId(39);
+pub const JUNGLE_POTION: QuestId = QuestId("jungle_potion");
 
-pub const THE_GRAND_TREE: QuestId = QuestId(40);
+pub const THE_GRAND_TREE: QuestId = QuestId("the_grand_tree");
 
-pub const SHILO_VILLAGE: QuestId = QuestId(41);
+pub const SHILO_VILLAGE: QuestId = QuestId("shilo_village");
 
-pub const UNDERGROUND_PASS: QuestId = QuestId(42);
+pub const UNDERGROUND_PASS: QuestId = QuestId("underground_pass");
 
-pub const OBSERVATORY_QUEST: QuestId = QuestId(43);
+pub const OBSERVATORY_QUEST: QuestId = QuestId("observatory_quest");
 
-pub const THE_TOURIST_TRAP: QuestId = QuestId(44);
+pub const THE_TOURIST_TRAP: QuestId = QuestId("the_tourist_trap");
 
-pub const WATCHTOWER: QuestId = QuestId(45);
+pub const WATCHTOWER: QuestId = QuestId("watchtower");
 
-pub const DWARF_CANNON: QuestId = QuestId(46);
+pub const DWARF_CANNON: QuestId = QuestId("dwarf_cannon");
 
-pub const MURDER_MYSTERY: QuestId = QuestId(47);
+pub const MURDER_MYSTERY: QuestId = QuestId("murder_mystery");
 
-pub const THE_DIG_SITE: QuestId = QuestId(48);
+pub const THE_DIG_SITE: QuestId = QuestId("the_dig_site");
 
-pub const GERTRUDES_CAT: QuestId = QuestId(49);
+pub const GERTRUDES_CAT: QuestId = QuestId("gertrudes_cat");
 
-pub const LEGENDS_QUEST: QuestId = QuestId(50);
+pub const LEGENDS_QUEST: QuestId = QuestId("legends_quest");
 
-pub const RUNE_MYSTERIES: QuestId = QuestId(51);
+pub const RUNE_MYSTERIES: QuestId = QuestId("rune_mysteries");
 
-pub const BIG_CHOMPY_BIRD_HUNTING: QuestId = QuestId(52);
+pub const BIG_CHOMPY_BIRD_HUNTING: QuestId = QuestId("big_chompy_bird_hunting");
 
-pub const ELEMENTAL_WORKSHOP_I: QuestId = QuestId(53);
+pub const ELEMENTAL_WORKSHOP_I: QuestId = QuestId("elemental_workshop_i");
 
-pub const PRIEST_IN_PERIL: QuestId = QuestId(54);
+pub const PRIEST_IN_PERIL: QuestId = QuestId("priest_in_peril");
 
-pub const NATURE_SPIRIT: QuestId = QuestId(55);
+pub const NATURE_SPIRIT: QuestId = QuestId("nature_spirit");
 
-pub const DEATH_PLATEAU: QuestId = QuestId(56);
+pub const DEATH_PLATEAU: QuestId = QuestId("death_plateau");
 
-pub const TROLL_STRONGHOLD: QuestId = QuestId(57);
+pub const TROLL_STRONGHOLD: QuestId = QuestId("troll_stronghold");
 
-pub const TAI_BWO_WANNAI_TRIO: QuestId = QuestId(58);
+pub const TAI_BWO_WANNAI_TRIO: QuestId = QuestId("tai_bwo_wannai_trio");
 
-pub const REGICIDE: QuestId = QuestId(59);
+pub const REGICIDE: QuestId = QuestId("regicide");
 
-pub const EADGARS_RUSE: QuestId = QuestId(60);
+pub const EADGARS_RUSE: QuestId = QuestId("eadgars_ruse");
 
-pub const SHADES_OF_MORTTON: QuestId = QuestId(61);
+pub const SHADES_OF_MORTTON: QuestId = QuestId("shades_of_mortton");
 
-pub const THE_FREMENNIK_TRIALS: QuestId = QuestId(62);
+pub const THE_FREMENNIK_TRIALS: QuestId = QuestId("the_fremennik_trials");
 
-pub const HORROR_FROM_THE_DEEP: QuestId = QuestId(63);
+pub const HORROR_FROM_THE_DEEP: QuestId = QuestId("horror_from_the_deep");
 
-pub const THRONE_OF_MISCELLANIA: QuestId = QuestId(64);
+pub const THRONE_OF_MISCELLANIA: QuestId = QuestId("throne_of_miscellania");
 
-pub const MONKEY_MADNESS_I: QuestId = QuestId(65);
+pub const MONKEY_MADNESS_I: QuestId = QuestId("monkey_madness_i");
 
-pub const HAUNTED_MINE: QuestId = QuestId(66);
+pub const HAUNTED_MINE: QuestId = QuestId("haunted_mine");
 
-pub const TROLL_ROMANCE: QuestId = QuestId(67);
+pub const TROLL_ROMANCE: QuestId = QuestId("troll_romance");
 
-pub const IN_SEARCH_OF_THE_MYREQUE: QuestId = QuestId(68);
+pub const IN_SEARCH_OF_THE_MYREQUE: QuestId = QuestId("in_search_of_the_myreque");
 
-pub const CREATURE_OF_FENKENSTRAIN: QuestId = QuestId(69);
+pub const CREATURE_OF_FENKENSTRAIN: QuestId = QuestId("creature_of_fenkenstrain");
 
-pub const ROVING_ELVES: QuestId = QuestId(70);
+pub const ROVING_ELVES: QuestId = QuestId("roving_elves");
 
-pub const GHOSTS_AHOY: QuestId = QuestId(71);
+pub const GHOSTS_AHOY: QuestId = QuestId("ghosts_ahoy");
 
-pub const ONE_SMALL_FAVOUR: QuestId = QuestId(72);
+pub const ONE_SMALL_FAVOUR: QuestId = QuestId("one_small_favour");
 
-pub const MOUNTAIN_DAUGHTER: QuestId = QuestId(73);
+pub const MOUNTAIN_DAUGHTER: QuestId = QuestId("mountain_daughter");
 
-pub const BETWEEN_A_ROCK: QuestId = QuestId(74);
+pub const BETWEEN_A_ROCK: QuestId = QuestId("between_a_rock");
 
-pub const THE_FEUD: QuestId = QuestId(75);
+pub const THE_FEUD: QuestId = QuestId("the_feud");
 
-pub const THE_GOLEM: QuestId = QuestId(76);
+pub const THE_GOLEM: QuestId = QuestId("the_golem");
 
-pub const DESERT_TREASURE: QuestId = QuestId(77);
+pub const DESERT_TREASURE: QuestId = QuestId("desert_treasure");
 
-pub const ICTHLARINS_LITTLE_HELPER: QuestId = QuestId(78);
+pub const ICTHLARINS_LITTLE_HELPER: QuestId = QuestId("icthlarins_little_helper");
 
-pub const TEARS_OF_GUTHIX: QuestId = QuestId(79);
+pub const TEARS_OF_GUTHIX: QuestId = QuestId("tears_of_guthix");
 
-pub const ZOGRE_FLESH_EATERS: QuestId = QuestId(80);
+pub const ZOGRE_FLESH_EATERS: QuestId = QuestId("zogre_flesh_eaters");
 
-pub const THE_LOST_TRIBE: QuestId = QuestId(81);
+pub const THE_LOST_TRIBE: QuestId = QuestId("the_lost_tribe");
 
-pub const THE_GIANT_DWARF: QuestId = QuestId(82);
+pub const THE_GIANT_DWARF: QuestId = QuestId("the_giant_dwarf");
 
-pub const RECRUITMENT_DRIVE: QuestId = QuestId(83);
+pub const RECRUITMENT_DRIVE: QuestId = QuestId("recruitment_drive");
 
-pub const MOURNINGS_END_PART_I: QuestId = QuestId(84);
+pub const MOURNINGS_END_PART_I: QuestId = QuestId("mournings_end_part_i");
 
-pub const FORGETTABLE_TALE_OF_A_DRUNKEN_DWARF: QuestId = QuestId(85);
+pub const FORGETTABLE_TALE_OF_A_DRUNKEN_DWARF: QuestId = QuestId("forgettable_tale_of_a_drunken_dwarf");
 
-pub const GARDEN_OF_TRANQUILLITY: QuestId = QuestId(86);
+pub const GARDEN_OF_TRANQUILLITY: QuestId = QuestId("garden_of_tranquillity");
 
-pub const A_TAIL_OF_TWO_CATS: QuestId = QuestId(87);
+pub const A_TAIL_OF_TWO_CATS: QuestId = QuestId("a_tail_of_two_cats");
 
-pub const WANTED: QuestId = QuestId(88);
+pub const WANTED: QuestId = QuestId("wanted");
 
-pub const MOURNINGS_END_PART_II: QuestId = QuestId(89);
+pub const MOURNINGS_END_PART_II: QuestId = QuestId("mournings_end_part_ii");
 
-pub const RUM_DEAL: QuestId = QuestId(90);
+pub const RUM_DEAL: QuestId = QuestId("rum_deal");
 
-pub const SHADOW_OF_THE_STORM: QuestId = QuestId(91);
+pub const SHADOW_OF_THE_STORM: QuestId = QuestId("shadow_of_the_storm");
 
-pub const MAKING_HISTORY: QuestId = QuestId(92);
+pub const MAKING_HISTORY: QuestId = QuestId("making_history");
 
-pub const RATCATCHERS: QuestId = QuestId(93);
+pub const RATCATCHERS: QuestId = QuestId("ratcatchers");
 
-pub const SPIRITS_OF_THE_ELID: QuestId = QuestId(94);
+pub const SPIRITS_OF_THE_ELID: QuestId = QuestId("spirits_of_the_elid");
 
-pub const DEVIOUS_MINDS: QuestId = QuestId(95);
+pub const DEVIOUS_MINDS: QuestId = QuestId("devious_minds");
 
-pub const THE_HAND_IN_THE_SAND: QuestId = QuestId(96);
+pub const THE_HAND_IN_THE_SAND: QuestId = QuestId("the_hand_in_the_sand");
 
-pub const ENAKHRAS_LAMENT: QuestId = QuestId(97);
+pub const ENAKHRAS_LAMENT: QuestId = QuestId("enakhras_lament");
 
-pub const CABIN_FEVER: QuestId = QuestId(98);
+pub const CABIN_FEVER: QuestId = QuestId("cabin_fever");
 
-pub const FAIRY_TALE_I_GROWING_PAINS: QuestId = QuestId(99);
+pub const FAIRY_TALE_I_GROWING_PAINS: QuestId = QuestId("fairy_tale_i_growing_pains");
 
-pub const RECIPE_FOR_DISASTER: QuestId = QuestId(100);
+pub const RECIPE_FOR_DISASTER: QuestId = QuestId("recipe_for_disaster");
 
-pub const IN_AID_OF_THE_MYREQUE: QuestId = QuestId(101);
+pub const IN_AID_OF_THE_MYREQUE: QuestId = QuestId("in_aid_of_the_myreque");
 
-pub const A_SOULS_BANE: QuestId = QuestId(102);
+pub const A_SOULS_BANE: QuestId = QuestId("a_souls_bane");
 
-pub const RAG_AND_BONE_MAN: QuestId = QuestId(103);
+pub const RAG_AND_BONE_MAN: QuestId = QuestId("rag_and_bone_man");
 
-pub const RAG_AND_BONE_MAN_II: QuestId = QuestId(104);
+pub const RAG_AND_BONE_MAN_II: QuestId = QuestId("rag_and_bone_man_ii");
 
-pub const SWAN_SONG: QuestId = QuestId(105);
+pub const SWAN_SONG: QuestId = QuestId("swan_song");
 
-pub const ROYAL_TROUBLE: QuestId = QuestId(106);
+pub const ROYAL_TROUBLE: QuestId = QuestId("royal_trouble");
 
-pub const DEATH_TO_THE_DORGESHUUN: QuestId = QuestId(107);
+pub const DEATH_TO_THE_DORGESHUUN: QuestId = QuestId("death_to_the_dorgeshuun");
 
-pub const FAIRY_TALE_II_CURE_A_QUEEN: QuestId = QuestId(108);
+pub const FAIRY_TALE_II_CURE_A_QUEEN: QuestId = QuestId("fairy_tale_ii_cure_a_queen");
 
-pub const LUNAR_DIPLOMACY: QuestId = QuestId(109);
+pub const LUNAR_DIPLOMACY: QuestId = QuestId("lunar_diplomacy");
 
-pub const THE_EYES_OF_GLOUPHRIE: QuestId = QuestId(110);
+pub const THE_EYES_OF_GLOUPHRIE: QuestId = QuestId("the_eyes_of_glouphrie");
 
-pub const DARKNESS_OF_HALLOWVALE: QuestId = QuestId(111);
+pub const DARKNESS_OF_HALLOWVALE: QuestId = QuestId("darkness_of_hallowvale");
 
-pub const THE_SLUG_MENACE: QuestId = QuestId(112);
+pub const THE_SLUG_MENACE: QuestId = QuestId("the_slug_menace");
 
-pub const ELEMENTAL_WORKSHOP_II: QuestId = QuestId(113);
+pub const ELEMENTAL_WORKSHOP_II: QuestId = QuestId("elemental_workshop_ii");
 
-pub const MY_ARMS_BIG_ADVENTURE: QuestId = QuestId(114);
+pub const MY_ARMS_BIG_ADVENTURE: QuestId = QuestId("my_arms_big_adventure");
 
-pub const ENLIGHTENED_JOURNEY: QuestId = QuestId(115);
+pub const ENLIGHTENED_JOURNEY: QuestId = QuestId("enlightened_journey");
 
-pub const EAGLES_PEAK: QuestId = QuestId(116);
+pub const EAGLES_PEAK: QuestId = QuestId("eagles_peak");
 
-pub const ANIMAL_MAGNETISM: QuestId = QuestId(117);
+pub const ANIMAL_MAGNETISM: QuestId = QuestId("animal_magnetism");
 
-pub const CONTACT: QuestId = QuestId(118);
+pub const CONTACT: QuestId = QuestId("contact");
 
-pub const COLD_WAR: QuestId = QuestId(119);
+pub const COLD_WAR: QuestId = QuestId("cold_war");
 
-pub const THE_FREMENNIK_ISLES: QuestId = QuestId(120);
+pub const THE_FREMENNIK_ISLES: QuestId = QuestId("the_fremennik_isles");
 
-pub const TOWER_OF_LIFE: QuestId = QuestId(121);
+pub const TOWER_OF_LIFE: QuestId = QuestId("tower_of_life");
 
-pub const THE_GREAT_BRAIN_ROBBERY: QuestId = QuestId(122);
+pub const THE_GREAT_BRAIN_ROBBERY: QuestId = QuestId("the_great_brain_robbery");
 
-pub const WHAT_LIES_BELOW: QuestId = QuestId(123);
+pub const WHAT_LIES_BELOW: QuestId = QuestId("what_lies_below");
 
-pub const OLAFS_QUEST: QuestId = QuestId(124);
+pub const OLAFS_QUEST: QuestId = QuestId("olafs_quest");
 
-pub const ANOTHER_SLICE_OF_HAM: QuestId = QuestId(125);
+pub const ANOTHER_SLICE_OF_HAM: QuestId = QuestId("another_slice_of_ham");
 
-pub const DREAM_MENTOR: QuestId = QuestId(126);
+pub const DREAM_MENTOR: QuestId = QuestId("dream_mentor");
 
-pub const GRIM_TALES: QuestId = QuestId(127);
+pub const GRIM_TALES: QuestId = QuestId("grim_tales");
 
-pub const KINGS_RANSOM: QuestId = QuestId(128);
+pub const KINGS_RANSOM: QuestId = QuestId("kings_ransom");
 
-pub const MONKEY_MADNESS_II: QuestId = QuestId(129);
+pub const MONKEY_MADNESS_II: QuestId = QuestId("monkey_madness_ii");
 
-pub const MISTHALIN_MYSTERY: QuestId = QuestId(130);
+pub const MISTHALIN_MYSTERY: QuestId = QuestId("misthalin_mystery");
 
-pub const CLIENT_OF_KOUREND: QuestId = QuestId(131);
+pub const CLIENT_OF_KOUREND: QuestId = QuestId("client_of_kourend");
 
-pub const BONE_VOYAGE: QuestId = QuestId(132);
+pub const BONE_VOYAGE: QuestId = QuestId("bone_voyage");
 
-pub const THE_QUEEN_OF_THIEVES: QuestId = QuestId(133);
+pub const THE_QUEEN_OF_THIEVES: QuestId = QuestId("the_queen_of_thieves");
 
-pub const THE_DEPTHS_OF_DESPAIR: QuestId = QuestId(134);
+pub const THE_DEPTHS_OF_DESPAIR: QuestId = QuestId("the_depths_of_despair");
 
-pub const THE_CORSAIR_CURSE: QuestId = QuestId(135);
+pub const THE_CORSAIR_CURSE: QuestId = QuestId("the_corsair_curse");
 
-pub const DRAGON_SLAYER_II: QuestId = QuestId(136);
+pub const DRAGON_SLAYER_II: QuestId = QuestId("dragon_slayer_ii");
 
-pub const TALE_OF_THE_RIGHTEOUS: QuestId = QuestId(137);
+pub const TALE_OF_THE_RIGHTEOUS: QuestId = QuestId("tale_of_the_righteous");
 
-pub const A_TASTE_OF_HOPE: QuestId = QuestId(138);
+pub const A_TASTE_OF_HOPE: QuestId = QuestId("a_taste_of_hope");
 
-pub const MAKING_FRIENDS_WITH_MY_ARM: QuestId = QuestId(139);
+pub const MAKING_FRIENDS_WITH_MY_ARM: QuestId = QuestId("making_friends_with_my_arm");
 
-pub const THE_FORSAKEN_TOWER: QuestId = QuestId(140);
+pub const THE_FORSAKEN_TOWER: QuestId = QuestId("the_forsaken_tower");
 
-pub const THE_ASCENT_OF_ARCEUUS: QuestId = QuestId(141);
+pub const THE_ASCENT_OF_ARCEUUS: QuestId = QuestId("the_ascent_of_arceuus");
 
-pub const X_MARKS_THE_SPOT: QuestId = QuestId(142);
+pub const X_MARKS_THE_SPOT: QuestId = QuestId("x_marks_the_spot");
 
-pub const SONG_OF_THE_ELVES: QuestId = QuestId(143);
+pub const SONG_OF_THE_ELVES: QuestId = QuestId("song_of_the_elves");
 
-pub const THE_FREMENNIK_EXILES: QuestId = QuestId(144);
+pub const THE_FREMENNIK_EXILES: QuestId = QuestId("the_fremennik_exiles");
 
 // Miniquests
 
-// TODO: We should probably assign the miniquests IDs in their release order.
+pub const ALFRED_GRIMHANDS_BARCRAWL: QuestId = QuestId("alfred_grimhands_barcrawl");
 
-pub const ALFRED_GRIMHANDS_BARCRAWL: QuestId = QuestId(-1);
+pub const ARCHITECTURAL_ALLIANCE: QuestId = QuestId("architectural_alliance");
 
-pub const ARCHITECTURAL_ALLIANCE: QuestId = QuestId(-2);
+pub const BEAR_YOUR_SOUL: QuestId = QuestId("bear_your_soul");
 
-pub const BEAR_YOUR_SOUL: QuestId = QuestId(-3);
+pub const CURSE_OF_THE_EMPTY_LORD: QuestId = QuestId("curse_of_the_empty_lord");
 
-pub const CURSE_OF_THE_EMPTY_LORD: QuestId = QuestId(-4);
+pub const ENCHANTED_KEY: QuestId = QuestId("enchanted_key");
 
-pub const ENCHANTED_KEY: QuestId = QuestId(-5);
+pub const ENTER_THE_ABYSS: QuestId = QuestId("enter_the_abyss");
 
-pub const ENTER_THE_ABYSS: QuestId = QuestId(-6);
+pub const FAMILY_PEST: QuestId = QuestId("family_pest");
 
-pub const FAMILY_PEST: QuestId = QuestId(-7);
+pub const THE_GENERALS_SHADOW: QuestId = QuestId("the_generals_shadow");
 
-pub const THE_GENERALS_SHADOW: QuestId = QuestId(-8);
+pub const IN_SEARCH_OF_KNOWLEDGE: QuestId = QuestId("in_search_of_knowledge");
 
-pub const IN_SEARCH_OF_KNOWLEDGE: QuestId = QuestId(-9);
+pub const LAIR_OF_TARN_RAZORLOR: QuestId = QuestId("lair_of_tarn_razorlor");
 
-pub const LAIR_OF_TARN_RAZORLOR: QuestId = QuestId(-10);
+pub const THE_MAGE_ARENA: QuestId = QuestId("the_mage_arena");
 
-pub const THE_MAGE_ARENA: QuestId = QuestId(-11);
+pub const THE_MAGE_ARENA_II: QuestId = QuestId("the_mage_arena_ii");
 
-pub const THE_MAGE_ARENA_II: QuestId = QuestId(-12);
-
-pub const SKIPPY_AND_THE_MOGRES: QuestId = QuestId(-13);
+pub const SKIPPY_AND_THE_MOGRES: QuestId = QuestId("skippy_and_the_mogres");
 
 fn all_quests() -> Vec<Quest> {
     use Skill::*;
